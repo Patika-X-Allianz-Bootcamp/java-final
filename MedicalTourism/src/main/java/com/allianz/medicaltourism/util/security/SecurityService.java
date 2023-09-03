ac06fd1e0875289e006f1178e9819804e32f0b3f
Ömer Faruk Tokluoğlu <88704993+Bersarks@users.noreply.github.com>
2023-09-03 23:08:28 +0300

    Update SecurityService.java

--- MedicalTourism/src/main/java/com/allianz/medicaltourism/util/security/SecurityService.java
+++ MedicalTourism/src/main/java/com/allianz/medicaltourism/util/security/SecurityService.java
@@ -1,6 +1,8 @@
 package com.allianz.medicaltourism.util.security;
 
+import com.allianz.medicaltourism.database.entity.RoleEntity;
 import com.allianz.medicaltourism.database.entity.UserEntity;
+import com.allianz.medicaltourism.database.repository.RoleEntityRepository;
 import com.allianz.medicaltourism.database.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,7 +12,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.Role;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,17 +23,27 @@ public class SecurityService implements UserDetailsService {
 
     @Autowired
     UserEntityRepository userEntityRepository;
+    @Autowired
+    RoleEntityRepository roleEntityRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<UserEntity> user = userEntityRepository.findByEmail(email);
-
+        List<RoleEntity> roles = roleEntityRepository.findAll();
         if (user.isEmpty()) {
             throw new UsernameNotFoundException("Could not findUser with email =" + email);
         }
-
+        for (RoleEntity role : roles) {
+            for(RoleEntity role1 : user.get().getRoles()){
+                if(role1.equals(role)){
+                    return new User(email,
+                            user.get().getPassword(),
+                            Collections.singletonList(new SimpleGrantedAuthority(role.getName())));
+                }
+            }
+        }
         return new User(email,
                 user.get().getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_user")));
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_anonymus")));
     }
 }
